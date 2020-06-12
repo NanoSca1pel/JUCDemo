60be89280b6335bf8525295609fa6730b810a15a
NanoSca1pel <lhtao846321@163.com>
2020-06-12 09:26:38 +0800

    修改错别字

--- src/main/java/com/juc/BlockingQueueDemo.java
+++ src/main/java/com/juc/BlockingQueueDemo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
  * @author lhtao
  *
  * ArrayBlockingQueue： 是一个基于数组结构的有界阻塞队列，此队列按FIFO（先进先出）的原则对元素进行排序。
- * LinkedBlockingQueue： 是一个基于链表结构的阻塞队列，此队列按FIFO（先进先出）排序原色，吞吐量通常要高于ArrayBlockingQueue。
+ * LinkedBlockingQueue： 是一个基于链表结构的阻塞队列，此队列按FIFO（先进先出）排序原则，吞吐量通常要高于ArrayBlockingQueue。
  * SynchronouseQueue： 是一个不存储元素的阻塞队列。每个插入操作必须等到另一个线程调用移除操作。否则插入操作一直处于阻塞状态，吞吐量通常要高于LinkedBlockingQueue
  *
  *
